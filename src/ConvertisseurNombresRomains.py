1fcdc4aabc75eaf4fe2e2917f3ae4406b9b56608
LoubatTh <75257332+LoubatTh@users.noreply.github.com>
2023-01-30 13:54:43 +0100

    Ajout Nombres romains

---
+++ src/ConvertisseurNombresRomains.py
@@ -0,0 +1,20 @@
+def convertir(nombre):
+    if nombre <= 3:
+        return 'I' * nombre
+
+    if nombre == 4:
+        return 'IV'
+
+    if nombre <= 8:
+        return 'V' + 'I' * (nombre - 5)
+
+    if nombre == 9:
+        return 'IX'
+
+    if nombre <= 13:
+        return 'X' + 'I' * (nombre - 10)
+
+    raise Exception('unexpected number')
+
+def cas_symbole_plus_n_unites(symbole, valeur_symbole, nombre_arabe):
+    return symbole + 'I' * (nombre_arabe - valeur_symbole)
---
+++ test/NombresRomainsTest.py
@@ -0,0 +1,59 @@
+import unittest
+import parameterized as parameterized
+import ConvertisseurNombresRomains
+
+
+class MyTestCase(unittest.TestCase):
+
+    @parameterized.parameterized.expand([[1], [2], [3]])
+    def test_one_to_three(self, nombre):
+        # ETANT DONNE un chiffre entre 1 et 3
+        # QUAND on le convertit en nombres romains
+        result = ConvertisseurNombresRomains.convertir(nombre)
+
+        # ALORS on obtient <n> fois 'I'
+        attendu = 'I' * nombre
+        self.assertEqual(attendu, result)
+
+    def test_four(self):
+        # ETANT DONNE le chiffre 4
+        nombre = 4
+
+        # QUAND on le convertit en nombres romains
+        result = ConvertisseurNombresRomains.convertir(nombre)
+
+        # ALORS on obtient 'IV'
+        self.assertEqual('IV', result)
+
+    @parameterized.parameterized.expand([[5], [6], [7], [8]])
+    def test_five_to_eight(self, nombre):
+        # ETANT DONNE un chiffre entre 5 et 8
+        # QUAND on le convertit en nombres romains
+        result = ConvertisseurNombresRomains.convertir(nombre)
+
+        # ALORS on obtient 'V' + <nombre>  fois 'I'
+        attendu = 'V' + 'I' * (nombre - 5)
+        self.assertEqual(attendu, result)
+
+    def test_nine(self):
+        # ETANT DONNE le chiffre 9
+        nombre = 9
+
+        # QUAND on le convertit en nombres romains
+        result = ConvertisseurNombresRomains.convertir(nombre)
+
+        # ALORS on obtient 'IX'
+        self.assertEqual('IX', result)
+
+    @parameterized.parameterized.expand([[10], [11], [12], [13]])
+    def test_ten_to_thirteen(self, nombre):
+        # ETANT DONNE un chiffre entre 10 à 13
+        # QUAND on le convertit en nombres romains
+        result = ConvertisseurNombresRomains.convertir(nombre)
+
+        # ALORS on obtient 'V' + <nombre>  fois 'I'
+        attendu = 'X' + 'I' * (nombre - 10)
+        self.assertEqual(attendu, result)
+
+if __name__ == '__main__':
+    unittest.main()
